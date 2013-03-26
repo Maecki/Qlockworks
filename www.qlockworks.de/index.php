e163473153471819ac74ab731166a34dc343255c
Benjamin.Hartl <benjamin.hartl@de.de>
2013-03-26 21:29:20 +0100

    Start

--- www.qlockworks.de/index.php
+++ www.qlockworks.de/index.php
@@ -1,11 +1,26 @@
 <?php
 /**
- * Copyright 2013 @ Qlockworks
+ * Copyright 2013 @ BWmedien GmbH
  * 
  * Author : Benjamin Hartl
- * Date : 2013-03-26
+ * Date : 2013-02-23
  */
 
-echo "Hello World";
+include("include/core.inc.php");
 
+$skeleton = new QwSkeleton();
+$skeleton->setSelect(QwSkeleton::HOME);
+$skeleton->setTitle("Startseite");
+$skeleton->begin();
+
+$pm = new QwProjectManager();
+$pm->execute(true);
+$pa = $pm->getObjects();
+if(count($pa) > 0){
+	
+}else{
+	echo 'Folgt in k&uuml;rze ...';
+}
+
+$skeleton->end();
 ?>
